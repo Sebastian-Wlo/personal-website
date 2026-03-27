79041145ff15efbdb2c7cdd34c345fea5f6285ce
Sebastian Włodarczyk <sebastianwlodarczyk92@protonmail.com>
2026-03-27 18:33:22 +0100

    feat: first version of the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    locales: ["en", "pl"],
+    defaultLocale: "en",
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
